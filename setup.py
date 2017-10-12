27f8616f689ad5fc81b655de97441d7e87d07ff5
petrux <giulio.petrucci@gmail.com>
2017-10-12 12:43:07 +0200

    fixed indentation.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(name='deon',
-      version='0.0.1',
-      description='Definition or not?',
-      url='https://github.com/dkmfbk/deon',
-      author='Giulio Petrucci (petrux)',
-      author_email='giulio.petrucci@gmail.com',
-      license='Apache License 2.0',
-      packages=find_packages(exclude=["tests"]),
-      install_requires=[],
-      dependency_links=[],
-      zip_safe=False)
+    version='0.0.1',
+    description='Definition or not?',
+    url='https://github.com/dkmfbk/deon',
+    author='Giulio Petrucci (petrux)',
+    author_email='giulio.petrucci@gmail.com',
+    license='Apache License 2.0',
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[],
+    dependency_links=[],
+    zip_safe=False)
