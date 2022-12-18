701219e89eaebbdcbacd787f1c1ae61e86780f3f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-18 17:56:09 +0000

    Adding tarfile member sanitization to extractall()

--- deon/data/wcldata/wcldata.py
+++ deon/data/wcldata/wcldata.py
@@ -26,7 +26,26 @@ class WCLDataSource(DataSource):
             with open(f_path, 'wb') as f_out:
                 f_out.write(urllib.request.urlopen(self._LINK).read())
             with tarfile.open(f_path, 'r:gz') as targz:
-                targz.extractall(dest)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(targz, dest)
 
         if util.tsv_already_exist(dest, [self._OUT_DEF_FILE, self._OUT_NODEF_FILE]):
             return
